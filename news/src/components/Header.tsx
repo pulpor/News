25969c1f831fca83a5d04180f7de8dd022e8715b
Leonardo Pulpor <pulppor@hotmail.com>
2023-11-27 11:34:16 -0300

    Changed the logo and publication name from Trybe News to Pulpor News

--- news/src/components/Header.tsx
+++ news/src/components/Header.tsx
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom'
-import logo from '../images/logo.png'
+import logo from '../images/head2.png'
 
 export function Header() {
   return (
     <header data-testid="header">
       
       <Link id="link" to="/"><div className="containerHeader">
-        <img src={logo} id="logo" alt="logo" />
+        <img src={logo} id="logo" alt="logo" width={180}/>
         <div id="titleOrg">
-          <p id="headerTitle">TRYBE NEWS</p>
+          <p id="headerTitle">PULPOR NEWS</p>
         </div> </div>
       </Link>
      
