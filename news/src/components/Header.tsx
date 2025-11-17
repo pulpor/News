6f84397517eb7bd309a2446cd9028ef50ce0fbcd
Leonardo Pulpor <pulppor@gmail.com>
2025-11-17 12:02:11 -0300

    feat: update header logo and styles, enhance global CSS for better layout and typography

--- news/src/components/Header.tsx
+++ news/src/components/Header.tsx
@@ -1,19 +1,20 @@
 import { Link } from 'react-router-dom'
-import logo from '../images/head2.png'
+import logo from '../images/logo.png'
 
 export function Header() {
   return (
     <header data-testid="header">
-      
-      <Link id="link" to="/"><div className="containerHeader">
-        <img src={logo} id="logo" alt="logo" width={180}/>
-        <div id="titleOrg">
-          <p id="headerTitle">PULPOR NEWS</p>
-        </div> </div>
+      <Link id="link" to="/">
+        <div className="containerHeader">
+          <div className="logoContainer">
+            <img src={logo} id="logo" alt="Pulpor News Logo" />
+          </div>
+          <div id="titleOrg">
+            <h1 id="headerTitle">PULPOR NEWS</h1>
+            <p className="headerSubtitle">Daily News & Reports</p>
+          </div>
+        </div>
       </Link>
-     
     </header>  
-   
   )
-
 }
