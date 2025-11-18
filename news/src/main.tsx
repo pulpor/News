52851b43fd537359e78d2c6da4ba520bc35befb8
Leonardo Pulpor <pulppor@gmail.com>
2025-11-18 10:19:46 -0300

    Add basename to BrowserRouter for GitHub Pages

--- news/src/main.tsx
+++ news/src/main.tsx
@@ -4,8 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/News">
     <App />
   </BrowserRouter>
 )
-
